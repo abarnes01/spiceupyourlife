d6af576b9e24f7b44a211af58241ba8d4133247d
adamjbarnes <adamjbarnes@192.168.0.35>
2021-02-03 12:39:38 +0000

    Added rest of Spring files

---
+++ Project Code/group15project/src/test/java/com/group15/Group15projectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group15projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

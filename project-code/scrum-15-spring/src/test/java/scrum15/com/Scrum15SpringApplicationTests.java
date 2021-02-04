25129282abaa49f5470ac34be96bd9030c304ce4
Adam Barnes <adamjbarnes@Adams-MacBook-Pro.local>
2021-02-04 14:22:25 +0000

    Added Spring Boot Project

---
+++ project-code/scrum-15-spring/src/test/java/scrum15/com/Scrum15SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package scrum15.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scrum15SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

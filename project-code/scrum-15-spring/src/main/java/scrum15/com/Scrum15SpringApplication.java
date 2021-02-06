ae87f578ebfa161d5078e41a1a74a0f193c0840b
Adam Barnes <adamjbarnes@Adams-MacBook-Pro.local>
2021-02-06 17:34:50 +0000

    Added scrum-15-spring files

---
+++ project-code/scrum-15-spring/src/main/java/scrum15/com/Scrum15SpringApplication.java
@@ -0,0 +1,13 @@
+package scrum15.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scrum15SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scrum15SpringApplication.class, args);
+	}
+
+}

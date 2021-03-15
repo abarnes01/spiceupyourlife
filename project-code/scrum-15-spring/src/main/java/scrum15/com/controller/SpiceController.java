72e75788e6a078aef52dd2899218d761042068e6
E Ulrich-Oltean <413-efuo1@users.noreply.campus.cs.le.ac.uk>
2021-03-15 18:13:12 +0000

    Upload Spice Controller (only cardamom currently)

---
+++ project-code/scrum-15-spring/src/main/java/scrum15/com/controller/SpiceController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SpiceController {
+	
+	@RequestMapping("/cardamom")
+	public String cardamom(@RequestParam int quantity, @RequestParam float type, Model model) {
+		
+		model.addAttribute("quantity", quantity);
+		
+		String plural = "s";
+		if (quantity == 1) {
+			plural = "";
+		}
+		if (type == 1) {
+			model.addAttribute("type", "Jar" + plural);
+		} else {
+			model.addAttribute("type", "Bag" + plural);
+		}		
+		
+		double Pcard = 3.49;
+		String price = String.format("%.02f", (Pcard * quantity) / type);
+		model.addAttribute("price", price);
+		
+		return "spices/cardamom";
+	}
+	
+	
+}
